0d2d2a0001d74dd280261780ce50a9c220402b91
AlexandreBBCH <70708976+AlexandreBBCH@users.noreply.github.com>
2023-10-10 16:12:08 +0200

    Ajout texture globals

--- ForestSurvivor/ForestSurvivor/AllGlobals/GlobalsTexture.cs
+++ ForestSurvivor/ForestSurvivor/AllGlobals/GlobalsTexture.cs
@@ -14,7 +14,8 @@ namespace ForestSurvivor.AllGlobals
         /// </summary>
         public static SpriteFont font;
         public static SpriteFont titleFont;
-        public static SpriteFont textGamefont;
+        public static SpriteFont textGamefont; 
+        public static SpriteFont lvlInfoFont;
         public static Texture2D MainMenu2D;
         public static Texture2D PauseBackground2D;
         public static Texture2D Minus;
@@ -26,8 +27,6 @@ namespace ForestSurvivor.AllGlobals
         public static Texture2D SlimeShooter2D;
         public static Texture2D SlimeShooterAmmo;
         public static Texture2D BigSlime2D;
-
-
         /// <summary>
         /// Items
         /// </summary>
@@ -41,6 +40,8 @@ namespace ForestSurvivor.AllGlobals
         /// Environment/Spawner
         /// </summary>
         public static Texture2D Bush;
+        public static Texture2D BushSpriteSheet;
+        public static Texture2D BushBerriesSpriteSheet;
         public static Texture2D Rock;
         public static Texture2D Background2D;
         /// <summary>
@@ -50,5 +51,10 @@ namespace ForestSurvivor.AllGlobals
         public static Texture2D bullet;
         public static Texture2D back;
         public static Texture2D front;
+        /// <summary>
+        /// Level card
+        /// </summary>
+        public static Texture2D cardInfos;
+        public static Texture2D cardView;
     }
 }
